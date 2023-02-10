35ef02ced836144d8278a927c1832b67d758f859
NickBilbrey <NicholasBilbrey18@gmail.com>
2023-02-10 17:13:41 -0500

    Add files via upload

---
+++ CirclesLab1/CirclesLab/Circles.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CirclesLab
+{
+    public class Circles
+    {
+        private double Radius;
+        public Circles(double radius)
+        {
+            Radius = radius;
+           
+        }
+        public double CalculatCircumference()
+        {
+            double circumference = 2 * Radius * Math.PI;
+            return circumference;
+        }
+        public string CalculatedFormattedCircumference()
+        {
+            double roundedCircumference = Math.Round(CalculatCircumference(),2);
+            return roundedCircumference.ToString();
+
+        }
+        public double CalculateArea()
+        {
+            double area = Math.PI * Radius * Radius;
+            return area;
+        }
+     
+
+        public string CalculateFormattedArea()
+        {
+            double roundedarea = Math.Round(CalculateArea(), 2);
+            return roundedarea.ToString();
+
+        }
+        /*private string FormatNumber(double x)
+        {
+
+        }
+*/
+
+
+
+    }
+}
---
+++ CirclesLab1/CirclesLab/Program.cs
@@ -0,0 +1,74 @@
+﻿
+
+
+
+using CirclesLab;
+
+Console.WriteLine("Welcome to my circle tester");
+
+bool validNum = true;
+bool makeMoreCircles = false;
+double userRadius = 0;
+string circumference;
+string area;
+do
+{
+   
+    do
+    {
+        Console.WriteLine("Please enter a radius: ");
+        string userRadiusAsString = Console.ReadLine();
+
+        try
+        {
+            userRadius = double.Parse(userRadiusAsString);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Please enter a valid number\n");
+            validNum = false;
+        }
+
+         
+
+         var passRadius = new Circles(userRadius);
+
+         circumference = passRadius.CalculatedFormattedCircumference();
+
+         area = passRadius.CalculateFormattedArea();
+         
+        
+
+
+
+    } while (validNum == false);
+
+    Console.WriteLine("Circumference: " + circumference);
+
+    Console.WriteLine("Area: " + area);
+
+    List<Circles> userCircles = new List<Circles>()
+    {
+        new Circles(userRadius)
+    };
+
+
+    int userCount = userCircles.Count() + 1;
+
+
+    Console.WriteLine("Would you like to make another Circle? enter y to continue or any other key to exit. ");
+    string userContinue = Console.ReadLine();
+
+    if (userContinue == "y")
+    {
+        makeMoreCircles = false;
+    }
+    else
+    {
+        Console.WriteLine("Goodbye");
+        Console.WriteLine($"You have made {userCount} Circles");
+        makeMoreCircles = true;
+    }
+
+}
+while (makeMoreCircles == false);
